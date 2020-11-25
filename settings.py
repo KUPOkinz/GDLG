cbdafa42d56e278f0b2fc9492bf7d85c9071aa3c
KUPOkinz <63320687+KUPOkinz@users.noreply.github.com>
2020-11-25 18:45:14 -0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+
+file_of_links = 'links.txt'
+
+email = "NULL"
+password = "NULL"
+student_id_number = "NULL"
+
+# PARAMETERS YOU NEED TO CHANGE ARE BELOW
+xml_filename = r"C:/Users/NULL/Desktop/Games.xml"
+gecko_driver_path = r"C:/NULL/Kodi Game Downloader/geckodriver.exe"
+firefox_binary_location = FirefoxBinary(r"C:/Program Files/Mozilla Firefox/firefox.exe")
